2589f2bc4f6d9122b92ac6ece8f306702719afb4
Roman Kamyk <roman.kamyk@gmail.com>
2013-07-23 13:19:04 -0700

    redesign + another test

--- 1ruby/bowling-kata/lib/bowling.rb
+++ 1ruby/bowling-kata/lib/bowling.rb
@@ -2,11 +2,52 @@
 
 # http://butunclebob.com/ArticleS.UncleBob.TheBowlingGameKata
 
+class Frame
+  def initialize
+    @rolls = []
+  end
+
+  def add(pins)
+    @rolls << pins
+  end
+
+  def finished?
+    score == 10 || @rolls.size >= 2
+  end
+
+  def score(last = nil)
+    last ||= Frame.new
+    sum = @rolls.inject(0) { |s, i| s + i }
+    sum += @rolls[0] || 0 if last.spare?
+    sum += @rolls[1] || 0 if last.strike?
+    sum
+  end
+
+  def spare?
+    @rolls.size > 1 && score == 10
+  end
+
+  def strike?
+    @rolls.first == 10
+  end
+end
+
 class Bowling
+  def initialize
+    @frames = []
+  end
+
   def roll(pins)
+    frame = @frames.last
+    if frame.nil? || frame.finished?
+      frame = Frame.new
+      @frames << frame
+    end
+    frame.add(pins)
   end
 
   def score
-    0
+    last = nil
+    @frames.inject(0) { |s, i| s += i.score(last); last = i; s }
   end
 end
--- 1ruby/bowling-kata/spec/lib/bowling_spec.rb
+++ 1ruby/bowling-kata/spec/lib/bowling_spec.rb
@@ -14,5 +14,23 @@ describe Bowling, "#score" do
     20.times { bowling.roll(1) }
     bowling.score.should eq(20)
   end
+
+  it "returns 16 for game with a spare" do
+    bowling = Bowling.new
+    bowling.roll(5);
+    bowling.roll(5); # spare
+    bowling.roll(1);
+    17.times { bowling.roll(0) }
+    bowling.score.should eq(5 + 5 + 1 + 1)
+  end
+
+  it "returns 12 for game with a spare" do
+    bowling = Bowling.new
+    bowling.roll(10); # strike
+    bowling.roll(1);
+    bowling.roll(1);
+    17.times { bowling.roll(0) }
+    bowling.score.should eq(10 + 1 + 1 + 1 + 1)
+  end
 end
 
