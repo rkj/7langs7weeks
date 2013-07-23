ec5acc80294e761b598ee1042b7f0778642c553a
Roman Kamyk <roman.kamyk@gmail.com>
2013-07-23 15:29:44 -0700

    working

--- 1ruby/bowling-kata/lib/bowling.rb
+++ 1ruby/bowling-kata/lib/bowling.rb
@@ -15,20 +15,35 @@ class Frame
     score == 10 || @rolls.size >= 2
   end
 
-  def score(last = nil)
-    last ||= Frame.new
-    sum = @rolls.inject(0) { |s, i| s + i }
-    sum += @rolls[0] || 0 if last.spare?
-    sum += @rolls[1] || 0 if last.strike?
-    sum
+  def sum
+    @rolls.inject(0) { |s, i| s + i }
+  end
+
+  def score(next_frame = nil, next_next = nil)
+    tmp = sum()
+    tmp += next_frame.first || 0 if next_frame && (spare? || strike?)
+    tmp += next_frame.second || next_next.first || 0 if next_frame && strike?
+    tmp
   end
 
   def spare?
-    @rolls.size > 1 && score == 10
+    @rolls.size > 1 && sum == 10
   end
 
   def strike?
     @rolls.first == 10
+  end
+
+  def first
+    @rolls.first
+  end
+
+  def second
+    @rolls[1]
+  end
+
+  def inspect
+    "<Frame #{@rolls.inspect} sum: #{sum} spare: #{spare?} strike: #{strike?}>"
   end
 end
 
@@ -47,7 +62,9 @@ class Bowling
   end
 
   def score
-    last = nil
-    @frames.inject(0) { |s, i| s += i.score(last); last = i; s }
+    sum = 0
+    @frames << Frame.new while @frames.size <= 15
+    @frames.each_cons(3) { |frame, n1, n2| sum += frame.score(n1, n2); p sum }
+    sum
   end
 end
--- 1ruby/bowling-kata/spec/lib/bowling_spec.rb
+++ 1ruby/bowling-kata/spec/lib/bowling_spec.rb
@@ -24,13 +24,29 @@ describe Bowling, "#score" do
     bowling.score.should eq(5 + 5 + 1 + 1)
   end
 
-  it "returns 12 for game with a spare" do
+  it "returns 12 for game with a strike" do
     bowling = Bowling.new
     bowling.roll(10); # strike
     bowling.roll(1);
     bowling.roll(1);
-    17.times { bowling.roll(0) }
+    18.times { bowling.roll(0) }
     bowling.score.should eq(10 + 1 + 1 + 1 + 1)
+  end
+
+  it "returns 35 for game with two strikes" do
+    bowling = Bowling.new
+    bowling.roll(10); # strike 10
+    bowling.roll(10); # strike 20 + 10
+    bowling.roll(1); # 20 + 11
+    bowling.roll(1); # 20 + 12
+    17.times { bowling.roll(0) }
+    bowling.score.should eq(21 + 12 + 2)
+  end
+
+  it "returns 300 for a perfect game" do
+    bowling = Bowling.new
+    12.times { bowling.roll(10) }
+    bowling.score.should eq(300)
   end
 end
 
