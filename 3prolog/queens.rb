0171e380a21eeba01fa6004384d16cb96670adb8
Roman Kamyk <roman.kamyk@gmail.com>
2013-09-03 16:12:43 -0700

    Shorthen the solution

--- 3prolog/queens.rb
+++ 3prolog/queens.rb
@@ -1,13 +1,11 @@
 #!/usr/bin/ruby
 
 def valid(solution)
-  return false until solution.length == 8
-  return false until solution.all? { |r, c| r >= 1 && r <= 8 && c >= 1 && c <= 8}
-  rows_covered = solution.map { |r, c| r }.uniq
-  cols_covered = solution.map { |r, c| c }.uniq
-  diags_covered = solution.map { |r, c| r + c}.uniq
-  diags2_covered = solution.map { |r, c| r - c}.uniq
-  return [rows_covered, cols_covered, diags_covered, diags2_covered].all? { |x| x.size == 8 }
+  rows = solution.map { |r, c| r }
+  cols = solution.map { |r, c| c }
+  diags = solution.map { |r, c| r + c}
+  diags2 = solution.map { |r, c| r - c}
+  return [rows, cols, diags, diags2].all? { |x| x.uniq.size == 8 }
 end
 
 solutions = (1..8).to_a.permutation.select do |p|
