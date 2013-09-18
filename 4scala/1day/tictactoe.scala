d0c1c522d812f92aa4ab137987cc94059d43b7ef
Roman Kamyk <roman.kamyk@gmail.com>
2013-09-17 17:17:42 -0700

    endgame printing

--- 4scala/1day/tictactoe.scala
+++ 4scala/1day/tictactoe.scala
@@ -2,12 +2,11 @@ object Field extends Enumeration {
   type Field = Value
   val X, O, E = Value
 }
+
 object Board {
   def parse(repr : String) = {
     repr.split("\n").map { line =>
-      //println("New Line: >" + line + "<")
       line.toCharArray.map { char =>
-        //println(char)
         char match {
           case 'X' => Field.X
           case 'O' => Field.O
@@ -15,6 +14,9 @@ object Board {
         }
       }
     }
+  }
+  def toString(arr : Array[Array[Field.Value]]) = {
+    arr.map { row => row.mkString(" ") }.mkString("\n")
   }
 }
 
@@ -24,36 +26,48 @@ class Board(val arr : Array[Array[Field.Value]] ) {
   }
   require(arr.size == 3)
 
-  def rows() = {
+  def rows = {
     arr
   }
-  def cols() = {
+  def cols = {
     arr transpose
   }
-  def diags() = {
+  def diags = {
     Array(
       Array(arr(0)(0), arr(1)(1), arr(2)(2)),
       Array(arr(0)(2), arr(1)(1), arr(2)(0))
     )
   }
-  def win() = {
+  def win = {
     val triplets = rows ++ cols ++ diags
     triplets.map { triplet =>
       val d = triplet.distinct
       (d.size == 1 && d(0) != Field.E, d(0))
     }.filter(b => b._1)
   }
-  def toString(arr : Array[Array[Field.Value]]) = {
-    arr.map { row => row.mkString(" ") }.mkString("\n")
+  def state = {
+    val w = win
+    val e = rows.exists { row => row.exists { f => f == Field.E }}
+    if (!w.isEmpty) {
+      w(0)._2.toString + " won"
+    } else if (e){
+      "Game in progress"
+    } else {
+      "Draw"
+    }
   }
-  override def toString() = {
-    toString(arr)
+  override def toString = {
+    Board.toString(arr) + "\n- " + state + "\n"
   }
 }
 
 val b = new Board("XXX\nEOE\nEEO")
 println(b)
-println(b.toString(b.cols))
-println(b.toString(b.diags))
-println(b.win.mkString("\n"))
+//println(Board.toString(b.cols))
+//println(Board.toString(b.diags))
 //val b2 = new Board("XXXX\nEOEE\nEEOE\nEEEE")
+//val b3 = new Board("XXY\nEOE\nEEO")
+
+println(new Board("XEX\nOOO\nEEO"))
+println(new Board("XEX\nOEO\nEEO"))
+println(new Board("XOX\nOXO\nOXO"))
