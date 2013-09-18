efbdeb6ca63f02566d4e4ca64817ff41b6f7488b
Roman Kamyk <roman.kamyk@gmail.com>
2013-09-18 13:30:30 -0700

    misc tests

--- 4scala/test.scala
+++ 4scala/test.scala
@@ -1,4 +1,5 @@
 
+//implicit def intToString(x: Int) = x.toString
 class Test(name : String) {
   //name = name + name
 }
@@ -7,10 +8,39 @@ class Test2(val name : String) {
 }
 class Test3(var name : String) {
   name = name + name
+  def bla(x : String) {
+    println("A String: " + x)
+  }
+  def ibla[A](x : A)(implicit conv : A => String) {
+    println("A String: " + x)
+  }
 }
+class X$ {
+  def test { println("X$.test") }
+}
+object X {
+  def test { println("X.test") }
+}
+val x = new X$
+println(x.getClass.getName)
+println(X.getClass.getName)
+x.test
+
+object Test {
+  def static { println("Static method") }
+}
+
 val a = new Test("bla")
+Test.static
+println(a.getClass.getName)
+println(Test.getClass.getName)
+//println(Class.forName("Test$") == Test)
 //println(a.name)
 val a2 = new Test2("bla")
 println(a2.name)
 val a3 = new Test3("bla")
 println(a3.name)
+a3.bla(10 + "a")
+//a3.ibla(10)
+//a3.bla(10)
+
